fa0c0bd402df17a84d272d6038c5a238049d6f3f
abhijitsingh86 <enggabhijit.singh@gmail.com>
2017-01-07 23:07:18 +0100

    Network proxy completed

--- app/network/NetworkProxy.scala
+++ app/network/NetworkProxy.scala
@@ -1,16 +1,32 @@
 package network
 
+import javax.inject.Inject
+
 import db.dao.SlickSimulationDao
+import play.api.libs.ws._
 import models.{Assembly, Component}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 /**
   * Created by billa on 07.01.17.
   */
-class NetworkProxy {
+class NetworkProxy(@Inject ws:WSClient) {
   this: SlickSimulationDao =>
 
   def sendAssemblyDetails(url: String, assembly: Assembly, assemblyUrls: Map[Int, String]) = {
       //send http request using assemblies details
+    import play.api.libs.json._
+    import play.api.libs.functional.syntax._
+    val data = Json.obj("assemblyId" -> assembly.id,"assemblyName"->assembly.name
+    ,"url" -> assemblyUrls.get(assembly.id))
+    var status=0
+    do {
+      val req = Await.result(ws.url(url).post(data), Duration.Inf)
+      status = if(req.status !=200)status+1 else req.status
+    }while (status != 200 || status !=5)
+
 
   }
 
