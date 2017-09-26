55088edaf8afabae92df7a1d757cb279718772a1
abhijit singh <enggabhijit.singh@gmail.com>
2017-09-26 11:35:15 +0200

    Redux dev buggy dependency check removed

--- app/assets/javascripts/components/redux/store/configureStore.js
+++ app/assets/javascripts/components/redux/store/configureStore.js
@@ -9,7 +9,7 @@ export default function configureStore(initialState) {
         initialState,
         compose(
             applyMiddleware(thunk),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            window.devToolsExtension ? window.devToolsExtension() : f => f
         )
     );
 
